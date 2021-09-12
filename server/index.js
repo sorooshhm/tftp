c523ad6e7c48a36e288fc1d2b4a1334e2faa68c3
Soroosh hamedifar <72218950+sorooshhm@users.noreply.github.com>
2021-09-12 17:05:23 +0430

    adding RRQ

--- server/index.js
+++ server/index.js
@@ -4,58 +4,6 @@ const socket = udp.createSocket("udp4");
 const fs = require("fs");
 const path = require("path");
 
-// class HandlePacket {
-//   constructor(rinfo, socket, req) {
-//     this.req = req;
-//     this.socket = socket;
-//     this.rinfo = rinfo;
-//     this.block = 1;
-//     this._data = [];
-//   }
-//   read() {
-//     const [filename, _, mode, __] = this.req;
-//     const data = fs.readFileSync(path.join(__dirname, filename));
-//     this.division(data);
-//     console.log(this._data);
-//     this.send(this._data[0]);
-//   }
-//   write() {}
-//   data() {}
-//   ack() {
-//     console.log(this._data);
-//     const [block] = this.req;
-//     if (block != this.block) {
-//       return;
-//     }
-//     this.block += 1;
-//     if (this.block > this._data.length) {
-//       console.log("reading file finished");
-//     }
-//     this.send(this._data[this.block - 1]);
-//   }
-//   error() {}
-//   send(data) {
-//     this.socket.send(
-//       `3 ${this.block} ${data}`,
-//       this.rinfo.port,
-//       this.rinfo.address
-//     );
-//   }
-//   division(file) {
-//     const LEN = 512;
-//     while (file.length) {
-//       let i = file.lastIndexOf(32, LEN + 1);
-//       // If no space found, try forward search
-//       if (i < 0) i = file.indexOf(32, LEN);
-//       // If there's no space at all, take the whole string
-//       if (i < 0) i = file.length;
-//       // This is a safe cut-off point; never half-way a multi-byte
-//       this._data.push(file.slice(0, i));
-//       file = file.slice(i + 1); // Skip space (if any)    }
-//     }
-//   }
-// }
-
 socket.on("error", (err) => {
   console.log(err);
   socket.close();
